242d40b89df37da84745e750c23df33b7619fabc
Daniel <dmendez@ncsu.edu>
2021-02-15 21:37:35 -0500

    Animation starter code and implementation in temp.py.

--- Project1/animation.py
+++ Project1/animation.py
@@ -1,18 +1,7 @@
-# Starter code based on https://projects.raspberrypi.org/en/projects/temperature-log 
-# and https://github.com/raspberrypilearning/temperature-log
-from gpiozero import CPUTemperature
-from time import sleep, strftime, time
+import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import datetime as dt
-
-# Function to write temperature log
-def write_temp(temp):
-    with open("cpu_temp.csv", "a") as log:
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
-
-# Object to read temperature
-cpu = CPUTemperature()
+import tmp102
 
 # Create figure for plotting
 fig = plt.figure()
@@ -20,18 +9,18 @@ ax = fig.add_subplot(1, 1, 1)
 xs = []
 ys = []
 
+# Initialize communication with TMP102
+tmp102.init()
+
 # This function is called periodically from FuncAnimation
 def animate(i, xs, ys):
 
-    # Read temperature (Celsius)
-    temp_c = round(cpu.temperature, 2)
+    # Read temperature (Celsius) from TMP102
+    temp_c = round(tmp102.read_temp(), 2)
 
     # Add x and y to lists
     xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
     ys.append(temp_c)
-
-    # Log temperature
-    write_temp(temp_c)
 
     # Limit x and y lists to 20 items
     xs = xs[-20:]
@@ -44,9 +33,8 @@ def animate(i, xs, ys):
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
-    plt.title('Temperature over Time')
+    plt.title('TMP102 Temperature over Time')
     plt.ylabel('Temperature (deg C)')
-
 
 # Set up plot to call animate() function periodically
 ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
