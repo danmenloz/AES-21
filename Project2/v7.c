5368dc12d7ccbba00eeafbf85da2b6984165fbbe
Daniel <dmendez@ncsu.edu>
2021-03-19 02:07:14 -0400

    v7 generated pulse after event.

--- Project2/v7.c
+++ Project2/v7.c
@@ -14,19 +14,25 @@
 #include "tools-common.h"
 
 
-// static void handle_event(unsigned int line_offset, unsigned int event_type,
-// 			 struct timespec *ts)
-// {
-// 	char *evname;
+static void handle_event(unsigned int in_line, struct gpiod_line_bulk *out_lines,
+						 struct timespec *ts)
+{
+	int rv;
+	int values[1];
 
-// 	if (event_type == GPIOD_LINE_EVENT_RISING_EDGE)
-// 		evname = " RISING EDGE";
-// 	else
-// 		evname = "FALLING EDGE";
+	values[0] = 1; // on
+	rv = gpiod_line_set_value_bulk(out_lines, values);
+	if (rv)
+		die_perror("unable to set GPIO out_lines value");
 
-// 	printf("event: %s offset: %u timestamp: [%8ld.%09ld]\n",
-// 	       evname, offset, ts->tv_sec, ts->tv_nsec);
-// }
+	values[0] = 0; // off
+	rv = gpiod_line_set_value_bulk(out_lines, values);
+	if (rv)
+		die_perror("unable to set GPIO out_lines value");
+
+	// printf("event occured! pin: %u timestamp: [%8ld.%09ld]\n",
+	//        in_line, ts->tv_sec, ts->tv_nsec);
+}
 
 static void handle_signal(int signum UNUSED)
 {
@@ -37,12 +43,12 @@ int main(int argc, char **argv)
 {
 	unsigned int offsets[64], num_lines = 1, events_done = 0, x;
 	// bool watch_rising = false, watch_falling = false;
-	int flags = 0;
+	// int flags = 0;
 	struct timespec timeout = { 10, 0 };
 	int rv, i, y, event_type;
 	// struct mon_ctx ctx;
 	struct gpiod_chip *chip;
-	struct gpiod_line_bulk *lines, *evlines;
+	struct gpiod_line_bulk *in_lines, *evlines, *out_lines;
 	// char *end;
 	struct gpiod_line_request_config config;
 	struct gpiod_line *line;
@@ -55,14 +61,17 @@ int main(int argc, char **argv)
 	signal(SIGINT, handle_signal);
 	signal(SIGTERM, handle_signal);
 
-	offsets[0] = 27; //input
 
 	chip = chip_open_lookup("gpiochip0");
 	if (!chip)
 		die_perror("unable to open gpiochip0");
 
-	lines = gpiod_chip_get_lines(chip, offsets, 1);
-	if (!lines)
+
+	// *************** Input configuration ****************
+	offsets[0] = 27; //input GPIO27
+
+	in_lines = gpiod_chip_get_lines(chip, offsets, 1);
+	if (!in_lines)
 		die_perror("unable to retrieve GPIO lines from chip");
 
 	memset(&config, 0, sizeof(config));
@@ -73,7 +82,7 @@ int main(int argc, char **argv)
 	// config.flags = flags;
 	config.flags = 0;
 
-	rv = gpiod_line_request_bulk(lines, &config, NULL);
+	rv = gpiod_line_request_bulk(in_lines, &config, NULL);
 	if (rv)
 		die_perror("unable to request GPIO lines for events");
 
@@ -81,9 +90,31 @@ int main(int argc, char **argv)
 	if (!evlines)
 		die("out of memory");
 
+
+
+	// *************** Output configuration ****************
+	offsets[0] = 17; //output GPIO17
+	int values[] = {0}; // 1 - on, 0 - off
+
+	out_lines = gpiod_chip_get_lines(chip, offsets, 1);
+	if (!out_lines)
+		die_perror("unable to retrieve GPIO lines from chip");
+
+	memset(&config, 0, sizeof(config));
+
+	config.consumer = "gpioset";
+	config.request_type = GPIOD_LINE_REQUEST_DIRECTION_OUTPUT;
+	config.flags = 0;
+
+	rv = gpiod_line_request_bulk(out_lines, &config, values); // set initial values
+	if (rv)
+		die_perror("unable to request output lines");
+
+
+	// infinite loop
 	for (;;) {
 		gpiod_line_bulk_reset(evlines);
-		rv = gpiod_line_event_wait_bulk(lines, &timeout, evlines);
+		rv = gpiod_line_event_wait_bulk(in_lines, &timeout, evlines);
 		if (rv < 0)
 			die_perror("error waiting for events");
 		if (rv == 0)
@@ -100,22 +131,24 @@ int main(int argc, char **argv)
 				die_perror("error reading line events");
 
 			for (y = 0; y < rv; y++) {
-				// handle_event(gpiod_line_offset(line),
-					    //  events[y].event_type,
-					    //  &events[y].ts);
-				events_done++;
+				handle_event(gpiod_line_offset(line), // input lines event
+						out_lines, // output lines
+					    &events[y].ts); // event timestamp
+				// events_done++;
 
-				if (events_done >= 100){
-					printf("100 events \n");
-					events_done = 0;
-				}
+				// if (events_done >= 100){
+				// 	printf("100 events \n");
+				// 	events_done = 0;
+				// }
 			}
 		}
 	}
 
 done:
-	gpiod_line_release_bulk(lines);
-	gpiod_line_bulk_free(lines);
+	gpiod_line_release_bulk(in_lines);
+	gpiod_line_bulk_free(in_lines);
+	gpiod_line_release_bulk(out_lines);
+	gpiod_line_bulk_free(out_lines);
 	gpiod_line_bulk_free(evlines);
 	gpiod_chip_close(chip);
 
