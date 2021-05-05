c33b6e15103451b4f70ea9dad57042d0b0711967
dmendez <dmendez@ncsu.edu>
2021-05-05 00:24:33 -0400

    OPT buffer.

--- Project3/rpi-mmal/video_callback.c
+++ Project3/rpi-mmal/video_callback.c
@@ -93,114 +93,102 @@ void video_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
   static int debug_rect_X=1280/2+100, debug_rect_Y=720/2-100;
   // Default target color 
   static YUV_T target = {64, 120, 197}; //red
-  int num_matches;
   
   clock_gettime(CLOCK_MONOTONIC, &tcf);
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t1);
 
   MMAL_BUFFER_HEADER_T *new_buffer;
-  MMAL_BUFFER_HEADER_T *preview_new_buffer;
   MMAL_POOL_T *pool = (MMAL_POOL_T *) port->userdata;
 
   loop++;
 
-  preview_new_buffer = mmal_queue_get(preview_input_port_pool->queue);
-  if (preview_new_buffer) {
+  int Y_array_size = w*h;
+  int UV_array_size = Y_array_size/4;
+  int x, y; // coordinates
+
+  int i;
+  unsigned char * Y = buffer->data;
+  unsigned char * U = &(buffer->data[Y_array_size]);
+  unsigned char * V = &(buffer->data[Y_array_size + UV_array_size]);
+
+  // initialize YUV_IMAGE_T data structures describing images
+  YUV_Image_Init(&img, (unsigned char *) (buffer->data), w, h); // original image
+  YUV_Image_Init(&img2, img2_bitplanes, w, h); // extra space for modified image
     
-    int Y_array_size = w*h;
-    int UV_array_size = Y_array_size/4;
-    int x, y; // coordinates
+  // update target color
+  YUV_T center_color;
+  Get_Pixel_yuv(&img, img.half_w, img.half_h, &center_color);
+  if (show_data > 2)
+    printf("\nCenter pixel: (%d, %d, %d)\n", center_color.y, center_color.u, center_color.v); 
+  if (update_target_color) {
+    if (debug_rectangle)
+      target = red;
+    else
+      target = center_color;
+    update_target_color = 0;
+    printf("\nUpdated target color: (%d, %d, %d)\n", target.y, target.u, target.v); 
+  }
 
-    int i;
-    unsigned char * Y = preview_new_buffer->data;
-    unsigned char * U = &(preview_new_buffer->data[Y_array_size]);
-    unsigned char * V = &(preview_new_buffer->data[Y_array_size + UV_array_size]);
+  // invert Y channel
+  if (invert) { // Y: luminance.
+    // Invert Luminance, one word at a time
+    unsigned int * Y32 = (unsigned int * ) Y;
+    do {
+*Y32 ^= 0xffffffff;
+Y32++;
+    }  while (Y32 < (unsigned int *) U); // Note: U must be a multiple of 4
+  } else if (invert_rect) {
+    // Invert brightness of central rectangle, one byte at a time
+    for (y = h/2 - H_INVERT_RECT/2; y < h/2 + H_INVERT_RECT/2; y++) {
+for (x = w/2 - W_INVERT_RECT/2; x < w/2 + W_INVERT_RECT/2; x++) {
+  Y[y*w + x] ^= 0xff;
+}
+    }
+  }
 
-    // copy image components to preview_new_buffer
-    memcpy(preview_new_buffer->data, buffer->data, Y_array_size + 2*UV_array_size);
-    preview_new_buffer->length = buffer->length;
+  // debug rectangle
+  if (debug_rectangle){
+    if ((loop & 0x0F) == 0 && debug_rectangle>1){
+      debug_rect_X = rand()%(w-W_DEBUG_RECT) + W_DEBUG_RECT/2;  
+      debug_rect_Y = rand()%(h-H_DEBUG_RECT) + H_DEBUG_RECT/2; 
+    }
+    Draw_Rectangle(&img, debug_rect_X, debug_rect_Y, W_DEBUG_RECT, H_DEBUG_RECT, &red, 1);
+  }
+
+  // find area matching target color
+  int centroid_x, centroid_y, num_matches, offsetX, offsetY;
+  num_matches = find_chroma_matches(&img, &target, &centroid_x, &centroid_y, chroma_subsample_sep);
+
+  // draw center circles
+  draw_overlay_info(&img);
   
-    // initialize YUV_IMAGE_T data structures describing images
-    YUV_Image_Init(&img, (unsigned char *) (preview_new_buffer->data), w, h); // original image
-    YUV_Image_Init(&img2, img2_bitplanes, w, h); // extra space for modified image
-      
-    // update target color
-    YUV_T center_color;
-    Get_Pixel_yuv(&img, img.half_w, img.half_h, &center_color);
-    if (show_data > 2)
-      printf("\nCenter pixel: (%d, %d, %d)\n", center_color.y, center_color.u, center_color.v); 
-    if (update_target_color) {
-      if (debug_rectangle)
-        target = red;
-      else
-        target = center_color;
-      update_target_color = 0;
-      printf("\nUpdated target color: (%d, %d, %d)\n", target.y, target.u, target.v); 
+  // image stabilizaton
+  if (num_matches > 0) {  
+    // Show centroid
+    Draw_Circle(&img, centroid_x, centroid_y, 10, &white, 1);
+    offsetX = img.half_w - centroid_x;
+    offsetY = img.half_h - centroid_y;
+    if (show_data > 1) {
+printf("Match centroid at (%d, %d) for %d samples\n",
+        centroid_x, centroid_y, num_matches);
+printf("Offset = %d, %d\n", offsetX, offsetY);
     }
-
-    // invert Y channel
-    if (invert) { // Y: luminance.
-      // Invert Luminance, one word at a time
-      unsigned int * Y32 = (unsigned int * ) Y;
-      do {
-	*Y32 ^= 0xffffffff;
-	Y32++;
-      }  while (Y32 < (unsigned int *) U); // Note: U must be a multiple of 4
-    } else if (invert_rect) {
-      // Invert brightness of central rectangle, one byte at a time
-      for (y = h/2 - H_INVERT_RECT/2; y < h/2 + H_INVERT_RECT/2; y++) {
-	for (x = w/2 - W_INVERT_RECT/2; x < w/2 + W_INVERT_RECT/2; x++) {
-	  Y[y*w + x] ^= 0xff;
-	}
-      }
+    // Correct image position
+    if (imstab_digital) {
+// Copy bitplanes of image so far into another buffer
+YUV_Image_Copy(&img2, &img);
+//	target.y = 128; // overwrite 
+YUV_Image_Fill(&img, &target);
+YUV_Translate_Image(&img, &img2, offsetX, offsetY, 0);
     }
-
-    // debug rectangle
-    if (debug_rectangle){
-      if ((loop & 0x0F) == 0 && debug_rectangle>1){
-        debug_rect_X = rand()%(w-W_DEBUG_RECT) + W_DEBUG_RECT/2;  
-        debug_rect_Y = rand()%(h-H_DEBUG_RECT) + H_DEBUG_RECT/2; 
-      }
-      Draw_Rectangle(&img, debug_rect_X, debug_rect_Y, W_DEBUG_RECT, H_DEBUG_RECT, &red, 1);
+    if (imstab_servo) {
+Update_Servo(offsetX, offsetY);
     }
-
-    // find area matching target color
-    int centroid_x, centroid_y, offsetX, offsetY;
-    num_matches = find_chroma_matches(&img, &target, &centroid_x, &centroid_y, chroma_subsample_sep);
-
-    // draw center circles
-    draw_overlay_info(&img);
-    
-    // image stabilizaton
-    if (num_matches > 0) {  
-      // Show centroid
-      Draw_Circle(&img, centroid_x, centroid_y, 10, &white, 1);
-      offsetX = img.half_w - centroid_x;
-      offsetY = img.half_h - centroid_y;
-      if (show_data > 1) {
-	printf("Match centroid at (%d, %d) for %d samples\n",
-	       centroid_x, centroid_y, num_matches);
-	printf("Offset = %d, %d\n", offsetX, offsetY);
-      }
-      // Correct image position
-      if (imstab_digital) {
-	// Copy bitplanes of image so far into another buffer
-	YUV_Image_Copy(&img2, &img);
-	//	target.y = 128; // overwrite 
-	YUV_Image_Fill(&img, &target);
-	YUV_Translate_Image(&img, &img2, offsetX, offsetY, 0);
-      }
-      if (imstab_servo) {
-	Update_Servo(offsetX, offsetY);
-      }
-    }
-    
-    // Send modified image in new buffer to preview component
-    if (mmal_port_send_buffer(preview_input_port, preview_new_buffer) != MMAL_SUCCESS) {
-      printf("ERROR: Unable to send buffer \n");
-    }
-  } else {
-    printf("ERROR: mmal_queue_get (%d)\n", preview_new_buffer);
+  }
+  
+  // Send modified image in new buffer to preview component
+  if (mmal_port_send_buffer(preview_input_port, buffer) != MMAL_SUCCESS) {
+    printf("ERROR: Unable to send buffer \n");
   }
       
   mmal_buffer_header_release(buffer);
