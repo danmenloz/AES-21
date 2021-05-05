becf49b766ea42a82726e07e644d11f629147d6f
dmendez <dmendez@ncsu.edu>
2021-05-05 01:15:37 -0400

    OPT vmla.

--- Project3/rpi-mmal/video_callback.c
+++ Project3/rpi-mmal/video_callback.c
@@ -18,7 +18,6 @@
 #include <arm_neon.h>
 #include <arm_acle.h>
 
-
 #include "PCA9685_servo_driver.h"
 
 #define MIN_MAX_CENTROID 0
@@ -75,13 +74,8 @@ int find_chroma_matches_neon(YUV_IMAGE_T *restrict i, YUV_T *restrict tc, int *r
   for (y = sep/2; y <= h - sep/2; y += sep) { 
     for (x = 0; x < w; x += 16) {
       int16x8_t du, dv, mu, mv, sq_uv_diff;
-      uint16x8_t posX, posY, masked_pos_X, masked_pos_Y, mask;
-      uint8x8_t mask_vis;
+      uint16x8_t posX, posY, mask;
       
-      // read Y channel, upper row and lower row
-      Y_image_u = vld2_u8(&(i->bY[(y-1)*w + x]));
-      Y_image_l = vld2_u8(&(i->bY[y*w + x]));
-
       // read U and V channels
       U_image_8 = vld1_u8(&(i->bU[y/2*half_w + x/2]));
       V_image_8 = vld1_u8(&(i->bV[y/2*half_w + x/2]));
@@ -103,6 +97,10 @@ int find_chroma_matches_neon(YUV_IMAGE_T *restrict i, YUV_T *restrict tc, int *r
 
       // highlighting mask
       if (highlight_matches){
+        // read Y channel, upper row and lower row
+        Y_image_u = vld2_u8(&(i->bY[(y-1)*w + x]));
+        Y_image_l = vld2_u8(&(i->bY[y*w + x]));
+
         Y_image_u.val[0] = vbsl_u8(vmovn_u16(mask), vld1_dup_u8(&(marker.y)), Y_image_u.val[0]);
         Y_image_u.val[1] = vbsl_u8(vmovn_u16(mask), vld1_dup_u8(&(marker.y)), Y_image_u.val[1]);
         Y_image_l.val[0] = vbsl_u8(vmovn_u16(mask), vld1_dup_u8(&(marker.y)), Y_image_l.val[0]);
@@ -125,13 +123,11 @@ int find_chroma_matches_neon(YUV_IMAGE_T *restrict i, YUV_T *restrict tc, int *r
       posX = vaddq_u16(pos_X_inc, vld1q_dup_u16(&x));
       posY = vld1q_dup_u16(&y);
 
-      // mask pos
-      masked_pos_X = vmulq_u16(mask, posX);
-      masked_pos_Y = vmulq_u16(mask, posY);
+      // vector sums using mask
+      sum_pos_X = vmlaq_u16(sum_pos_X, mask, posX);
+      sum_pos_Y = vmlaq_u16(sum_pos_Y, mask, posY);
 
-      // vector sums
-      sum_pos_X = vaddq_u16(sum_pos_X, masked_pos_X);
-      sum_pos_Y = vaddq_u16(sum_pos_Y, masked_pos_Y);
+      // matches vector sum
       sum_matches = vaddq_u16(sum_matches, mask);
 
       // pos scalar sums have to happen inside the loop. O.W. lanes will overflow.
@@ -162,7 +158,7 @@ int find_chroma_matches_neon(YUV_IMAGE_T *restrict i, YUV_T *restrict tc, int *r
     }
   }
 
-  // scalar sum matches
+  // matches scalar sum
   // safe to sum here since lanes didn't overflow
   sum_u = vget_high_u16(sum_matches);
   sum_l = vget_low_u16(sum_matches);
@@ -189,11 +185,11 @@ int find_chroma_matches(YUV_IMAGE_T *restrict i, YUV_T *restrict tc, int *restri
   int matches=0;
   #if MIN_MAX_CENTROID
     unsigned int min_x=0xffffffff, max_x=0, min_y=0xffffffff, max_y=0;
-  #endif  
+  #endif
   int offsetX=0, offsetY=0;
   YUV_T color;
   int cx=0, cy=0;
-  
+
   int y_end = i->h - sep/2;
   int x_end = i->w - sep/2;
   
@@ -203,16 +199,16 @@ int find_chroma_matches(YUV_IMAGE_T *restrict i, YUV_T *restrict tc, int *restri
       // Identify pixels with right color
       int diff = Sq_UV_Difference_yuv(&color, tc);
       if (diff < color_threshold) {
-	cx += x;
-	cy += y;
-  #if MIN_MAX_CENTROID
-  min_x = MIN(min_x, x);
-  max_x = MAX(max_x, x);
-  min_y = MIN(min_y, y);
-  max_y = MAX(max_y, y);
-  #endif
+        cx += x;
+        cy += y;
+        #if MIN_MAX_CENTROID
+        min_x = MIN(min_x, x);
+        max_x = MAX(max_x, x);
+        min_y = MIN(min_y, y);
+        max_y = MAX(max_y, y);
+        #endif
 
-	matches++;
+        matches++;
         if (highlight_matches){
           if (sep > 10)
             Draw_Rectangle(i, x, y, sep-2, sep-2, &pink, 0);
@@ -259,7 +255,7 @@ void video_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
   MMAL_POOL_T *pool = (MMAL_POOL_T *) port->userdata;
 
   loop++;
-
+  
   int Y_array_size = w*h;
   int UV_array_size = Y_array_size/4;
   int x, y; // coordinates
@@ -272,7 +268,7 @@ void video_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
   // initialize YUV_IMAGE_T data structures describing images
   YUV_Image_Init(&img, (unsigned char *) (buffer->data), w, h); // original image
   YUV_Image_Init(&img2, img2_bitplanes, w, h); // extra space for modified image
-    
+  
   // update target color
   YUV_T center_color;
   Get_Pixel_yuv(&img, img.half_w, img.half_h, &center_color);
@@ -292,15 +288,15 @@ void video_buffer_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
     // Invert Luminance, one word at a time
     unsigned int * Y32 = (unsigned int * ) Y;
     do {
-*Y32 ^= 0xffffffff;
-Y32++;
+      *Y32 ^= 0xffffffff;
+      Y32++;
     }  while (Y32 < (unsigned int *) U); // Note: U must be a multiple of 4
   } else if (invert_rect) {
     // Invert brightness of central rectangle, one byte at a time
     for (y = h/2 - H_INVERT_RECT/2; y < h/2 + H_INVERT_RECT/2; y++) {
-for (x = w/2 - W_INVERT_RECT/2; x < w/2 + W_INVERT_RECT/2; x++) {
-  Y[y*w + x] ^= 0xff;
-}
+      for (x = w/2 - W_INVERT_RECT/2; x < w/2 + W_INVERT_RECT/2; x++) {
+        Y[y*w + x] ^= 0xff;
+      }
     }
   }
 
@@ -312,11 +308,11 @@ for (x = w/2 - W_INVERT_RECT/2; x < w/2 + W_INVERT_RECT/2; x++) {
     }
     Draw_Rectangle(&img, debug_rect_X, debug_rect_Y, W_DEBUG_RECT, H_DEBUG_RECT, &red, 1);
   }
-
+  
   // find area matching target color
   int centroid_x, centroid_y, num_matches, offsetX, offsetY;
   num_matches = find_chroma_matches_neon(&img, &target, &centroid_x, &centroid_y, chroma_subsample_sep);
-
+  
   // draw center circles
   draw_overlay_info(&img);
   
@@ -327,20 +323,20 @@ for (x = w/2 - W_INVERT_RECT/2; x < w/2 + W_INVERT_RECT/2; x++) {
     offsetX = img.half_w - centroid_x;
     offsetY = img.half_h - centroid_y;
     if (show_data > 1) {
-printf("Match centroid at (%d, %d) for %d samples\n",
-        centroid_x, centroid_y, num_matches);
-printf("Offset = %d, %d\n", offsetX, offsetY);
+      printf("Match centroid at (%d, %d) for %d samples\n",
+              centroid_x, centroid_y, num_matches);
+      printf("Offset = %d, %d\n", offsetX, offsetY);
     }
     // Correct image position
     if (imstab_digital) {
-// Copy bitplanes of image so far into another buffer
-YUV_Image_Copy(&img2, &img);
-//	target.y = 128; // overwrite 
-YUV_Image_Fill(&img, &target);
-YUV_Translate_Image(&img, &img2, offsetX, offsetY, 0);
+      // Copy bitplanes of image so far into another buffer
+      YUV_Image_Copy(&img2, &img);
+      //	target.y = 128; // overwrite 
+      YUV_Image_Fill(&img, &target);
+      YUV_Translate_Image(&img, &img2, offsetX, offsetY, 0);
     }
     if (imstab_servo) {
-Update_Servo(offsetX, offsetY);
+      Update_Servo(offsetX, offsetY);
     }
   }
   
